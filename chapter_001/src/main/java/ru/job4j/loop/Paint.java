d43713e684ebec7109cadbe9c98c858944f15be7
EgorVasilyev <deseng_pgs@mail.ru>
2018-10-02 21:17:01 +0300

    5.4. Построить пирамиду в псевдографике. [#4412]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,71 @@
+package ru.job4j.loop;
+
+/**
+ * @author Egor Vasilyev (deseng_pgs@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Paint {
+    /**Метод строит правую сторону пирамиды в псевдографике.
+     @param height Высота.
+     */
+    public String rightTrl(int height) {
+        // Буфер для результата.
+        StringBuilder screen = new StringBuilder();
+        // ширина равна высоте.
+        int weight = height;
+        // внешний цикл двигается по строкам.
+        for (int row = 0; row != height; row++) {
+            // внутренний цикл определяет положение ячейки в строке.
+            for (int column = 0; column != weight; column++) {
+                // если строка равна ячейки, то рисуем галку.
+                // в данном случае строка определяет, сколько галок будет на строке
+                if (row >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            // добавляем перевод строки.
+            screen.append(System.lineSeparator());
+        }
+        // Получаем результат.
+        return screen.toString();
+    }
+    /**Метод строит левую сторону пирамиды в псевдографике.
+     @param height Высота.
+     */
+    public String leftTrl(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = height;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= weight - column - 1) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+    /**Метод строит пирамиду в псевдографике.
+     @param height Высота.
+     */
+    public String pyramid(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = 2 * height - 1;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= height - column - 1 && row + height - 1 >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
+}
