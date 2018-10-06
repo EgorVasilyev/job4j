5045d29f04b15439fd5ca40795f1ea14e97cf2ff
EgorVasilyev <deseng_pgs@mail.ru>
2018-10-06 16:02:48 +0300

    6.4. Слово начинается с ... [#41585]

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -13,7 +13,7 @@ public class Turn {
      * @return результат переворота.
      */
     public int[] turn(int[] array) {
-        for (int index = 0; index < array.length/2; index++) {
+        for (int index = 0; index < array.length / 2; index++) {
             int temp = array[array.length - index - 1];
             array[array.length - index - 1] = array[index];
             array[index] = temp;
