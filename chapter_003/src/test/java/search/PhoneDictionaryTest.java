d5386fc20a4484ea4dc4271ab30ebfb7aaf3a9f0
EgorVasilyev <deseng_pgs@mail.ru>
2018-11-07 20:10:31 +0300

    1. Телефонный справочник на базе ArrayList [#41598]

---
+++ chapter_003/src/test/java/search/PhoneDictionaryTest.java
@@ -0,0 +1,21 @@
+package search;
+
+import org.junit.Test;
+import ru.job4j.search.Person;
+import ru.job4j.search.PhoneDictionary;
+
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindBySurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Egor", "Vasilyev", "9192225", "Moscow")
+        );
+        List<Person> persons = phones.find("ily");
+        assertThat(persons.iterator().next().getName(), is("Egor"));
+    }
+}
