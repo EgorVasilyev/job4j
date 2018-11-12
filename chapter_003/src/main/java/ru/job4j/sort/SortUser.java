a08bcd3576d64cdde765e5198f658db0124d0fd9
EgorVasilyev <deseng_pgs@mail.ru>
2018-11-12 20:56:48 +0300

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_003/src/main/java/ru/job4j/sort/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sort/SortUser.java
@@ -1,5 +1,6 @@
 package ru.job4j.sort;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -9,4 +10,40 @@ public class SortUser {
     public Set<User> sort(List<User> list) {
         return new TreeSet<User>(list);
     }
+
+    public List<User> sortNameLength(List<User> list) {
+        list.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        int result = 0;
+                        if ((o1.getName().length()) < (o2.getName().length())) {
+                            result = -1;
+                        } else if ((o1.getName().length()) > (o2.getName().length())) {
+                            result = 1;
+                        }
+                        return result;
+                    }
+                }
+        );
+        return list;
+    }
+
+    public List<User> sortByAllFields(List<User> list) {
+        list.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        int result = o1.getName().compareTo(o2.getName());
+                        if (result == 0) {
+                            result = o1.getAge().compareTo(o2.getAge());
+                        }
+                        return result;
+                    }
+                }
+        );
+        return list;
+    }
+
+
 }
--- chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sort/SortUserTest.java
@@ -24,4 +24,36 @@ public class SortUserTest {
         String result1 = resultUsers.toString();
         assertThat(result1, is("[Leva 2, Vika 21, Egor 25]"));
     }
+
+    @Test
+    public void whenListSortByLengthOfName() {
+        SortUser sortUser = new SortUser();
+        List<User> list = new ArrayList<>();
+        User user1 = new User(25, "Egor");
+        User user2 = new User(2, "Lev");
+        User user3 = new User(21, "Viktoria");
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+        List<User> resultUsers = sortUser.sortNameLength(list);
+        String result1 = resultUsers.toString();
+        assertThat(result1, is("[Lev 2, Egor 25, Viktoria 21]"));
+    }
+
+    @Test
+    public void whenListSortByAllFields() {
+        SortUser sortUser = new SortUser();
+        List<User> list = new ArrayList<>();
+        User user3 = new User(25, "Egor");
+        User user2 = new User(2, "Egor");
+        User user1 = new User(21, "Viktoria");
+        User user4 = new User(45, "Misha");
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+        list.add(user4);
+        List<User> resultUsers = sortUser.sortByAllFields(list);
+        String result1 = resultUsers.toString();
+        assertThat(result1, is("[Egor 2, Egor 25, Misha 45, Viktoria 21]"));
+    }
 }
