3ab3640ed9d4fe42ed1a7beb4a373a60692733d5
EgorVasilyev <deseng_pgs@mail.ru>
2019-04-22 22:49:53 +0300

    Фильтры[#92177]

---
+++ 2_chapter_003/src/main/java/sql/commands.sql
@@ -0,0 +1,22 @@
+--создать:
+----базу данных
+create database name_of_database;
+----таблицу
+create table name_of_table(
+  name_of_arg1 type_of_arg,
+  name_of_arg2 type_of_arg1,
+  name_of_arg3 type_of_arg2 references name_of_other_table(name_of_arg_of_other_table)
+);
+
+--добавить:
+----значение в таблицу:
+insert into name_of_table(name_of_arg, ...) values(value_according_type_of_arg, ...);
+
+--изменить:
+----таблицу
+------добавить столбец
+ALTER TABLE name_of_table ADD COLUMN name_of_arg;
+------добавить столбец, ссылающийся на столбец из другой таблицы
+ALTER TABLE name_of_table ADD COLUMN name_of_arg type_of_arg references name_of_other_table(name_of_arg_of_other_table);
+------изменить значение в столбце при условии
+update name_of_table set name_of_arg = value_of_arg where name_of_other_arg = value_of_other_arg;
