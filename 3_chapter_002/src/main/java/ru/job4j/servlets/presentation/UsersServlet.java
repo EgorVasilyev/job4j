380db534e3deb00e44d69d74a1f6542e38138737
EgorVasilyev <deseng_pgs@mail.ru>
2019-06-04 21:35:43 +0300

    1. Перенести все виды из предыдущего задания на JSP [#2515]

--- 3_chapter_002/src/main/java/ru/job4j/servlets/presentation/UsersServlet.java
+++ 3_chapter_002/src/main/java/ru/job4j/servlets/presentation/UsersServlet.java
@@ -1,6 +1,5 @@
 package ru.job4j.servlets.presentation;
 
-import ru.job4j.servlets.datamodel.User;
 import ru.job4j.servlets.logic.ValidateService;
 
 import javax.servlet.ServletException;
@@ -8,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class UsersServlet extends HttpServlet {
     private final ValidateService logic = ValidateService.getSingletonInstance();
@@ -18,38 +16,7 @@ public class UsersServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        PrintWriter printWriter = new PrintWriter(resp.getOutputStream());
-        StringBuilder users = new StringBuilder("<table>");
-        users.append("List of available users:");
-        for (User userValue : this.logic.findAll().values()) {
-            users.append("<tr><td>ID: " + userValue.getId() + "</tr></td>");
-            users.append("<tr><td>Name: " + userValue.getName() + "</tr></td>");
-            users.append("<tr><td>Login: " + userValue.getLogin() + "</tr></td>");
-            users.append("<tr><td>Email: " + userValue.getEmail() + "</tr></td>");
-
-            users.append("<tr><td><form action='" + req.getContextPath() + "/edit?id=" + userValue.getId() + "' method='post'>");
-            users.append("<input type='submit' value ='Edit'></form></td>");
-
-            users.append("<td><form action='" + req.getContextPath() + "/users' method='post'>");
-            users.append("<input type='hidden' name='action' value='delete'>");
-            users.append("<input type='hidden' name='id' value='" + userValue.getId() + "'/>");
-            users.append("<input type='submit' value ='Delete'>");
-            users.append("</form></tr></td>");
-        }
-        users.append("</table>");
-
-        printWriter.print("<!DOCTYPE html>" +
-                "<html lang=\"en\">" +
-                "<head>" +
-                "    <meta charset=\"UTF-8\">" +
-                "    <title></title>" +
-                "</head>" +
-                "<body>" +
-                users.toString() +
-                "</body>" +
-                "</html>");
-        printWriter.flush();
+        req.getRequestDispatcher("/WEB-INF/users.jsp").forward(req, resp);
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
