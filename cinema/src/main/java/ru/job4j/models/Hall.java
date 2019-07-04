c9198834b5cf7e7e280ab5cb69a12a71ef339f78
EgorVasilyev <deseng_pgs@mail.ru>
2019-07-05 02:02:59 +0300

    Сервис - Кинотеатр [#3236]

---
+++ cinema/src/main/java/ru/job4j/models/Hall.java
@@ -0,0 +1,87 @@
+package ru.job4j.models;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Hall {
+    private int id;
+    private int number;
+    private int countRanges;
+    private int countPlaces;
+    private List<Integer> blockedPlaces;
+
+    public Hall(int number, int countRanges, int countPlaces, List<Integer> blockedPlaces) {
+        this.number = number;
+        this.countRanges = countRanges;
+        this.countPlaces = countPlaces;
+        this.blockedPlaces = blockedPlaces;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getCountRanges() {
+        return countRanges;
+    }
+
+    public void setCountRanges(int countRanges) {
+        this.countRanges = countRanges;
+    }
+
+    public int getCountPlaces() {
+        return countPlaces;
+    }
+
+    public void setCountPlaces(int countPlaces) {
+        this.countPlaces = countPlaces;
+    }
+
+    public List<Integer> getBlockedPlaces() {
+        return blockedPlaces;
+    }
+
+    public void setBlockedPlaces(List<Integer> blockedPlaces) {
+        this.blockedPlaces = blockedPlaces;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Hall hall = (Hall) o;
+        return id == hall.id
+                && number == hall.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, number);
+    }
+
+    @Override
+    public String toString() {
+        return "Hall{"
+                + "number=" + number
+                + ", countRanges=" + countRanges
+                + ", countPlaces=" + countPlaces
+                + ", blockedPlaces=" + blockedPlaces
+                + '}';
+    }
+}
